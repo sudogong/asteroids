35d5c10c630d890db019477f02b6bc548414fd64
Andrei Deak <andrei.deak@icloud.com>
2026-03-13 13:05:57 +0100

    add player movement

--- constants.py
+++ constants.py
@@ -1,2 +1,13 @@
+# Screen size
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# Circle representing radius of spaceship (hitbox)
+PLAYER_RADIUS = 20
+
+# Width of spaceship
+LINE_WIDTH = 2
+
+# Spaceship speed measures
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,14 +10,31 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(f"Player: {player}")
+
+    # (Intended Infinite) Rendering Loop
     while True:
         log_state()
         for event in pygame.event.get():
             # Make the close button work
             if event.type == pygame.QUIT:
+                print("INFO - User successfully stopped the program")
                 return
+            
         screen.fill("black")
+
+        #Add a player
+        player.draw(screen=screen)
+
+        # Update player position on screen on each frame
+        player.update(dt=dt)
+
+        # Render next screen
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
     
 
